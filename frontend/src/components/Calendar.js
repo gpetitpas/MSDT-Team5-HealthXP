0bf63b5335ebf613d7cc173646689b3a1e8d78f8
gpetitpas <gpetitpas@users.noreply.github.com>
2022-03-24 18:05:30 -0400

    Add Kalend calendar npm library

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,25 +1,26 @@
 import React from "react";
 
+import Kalend, { CalendarView } from 'kalend' // import component
+import 'kalend/dist/styles/index.css'; // import styles
+
 function Calendar() {
   return (
     <div className="Calendar">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
           <div class="col-lg-5">
             <h1 class="text-white">Calendar</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <Kalend
+              events={[]}
+              initialDate={new Date().toISOString()}
+              hourHeight={60}
+              initialView={CalendarView.WEEK}
+              disabledViews={[CalendarView.DAY]}
+              timeFormat={'24'}
+              weekDayStart={'Monday'}
+              calendarIDsHidden={['work']}
+              language={'en'}
+            />
           </div>
         </div>
       </div>
@@ -31,4 +32,5 @@ export default Calendar;
 
 /*
 Upgrade npm modules and try using this calendar npm package: https://reactjsexample.com/react-calendar-component-with-support-for-multiple-views-and-events/
+Here's the github for the kalend npm package: https://github.com/nibdo/kalend
 */
